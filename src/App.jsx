6342071525fbcf866c32c5d369ed0109ae247811
Paul Charaja Torres <68834265+ch4r4j4@users.noreply.github.com>
2024-01-30 22:58:10 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+
+
+import { Button } from "keep-react";
+  const App=()=>{
+    return (
+      <Button size="md">Default</Button>
+    )
+  }
+export default App;

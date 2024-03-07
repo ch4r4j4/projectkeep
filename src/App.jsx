fa022b0f262c5dac39d8a3a01bb2e8e69ddd11e2
Paul Charaja Torres <68834265+ch4r4j4@users.noreply.github.com>
2024-03-06 23:54:19 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,4 +16,6 @@ const App=()=>{
   }
 export default App;
 
-/* utilizando todos los cambios para terminar todo esta locura*/
+/* utilizando todos los cambios para terminar todo esta locura
+vamos avanzando poco a poco para ver como solucionamos estas cosas se que con fe lo logro
+*/

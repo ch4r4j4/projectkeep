118114d274fa7cffdab29d5b47bf4b90b6d92f88
Paul Charaja Torres <68834265+ch4r4j4@users.noreply.github.com>
2024-03-31 21:53:25 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,6 @@ const App=()=>{
   }
 export default App;
 
-/* utilizando todos los cambios para terminar todo esta locura
-poco a poco seguiremos avanzando
-solo para que nuestro github aprezaca que estamos avanzando
-vamos avanzando poco a poco para ver como solucionamos estas cosas se que con fe lo logro
-continuemos asi y seguir dandole
-vamos a terminar ese componente sea como sea
-vamo a vanzando poco a poco hoy subimos y logramos dividr el path y utilizar de mejor manera el app
-poco a poco me has querido poco a poco me has amado morenita de mi amor nunca diga sque no son cossas del amor
-a seguir dandloe cambio antes de continuar nuestro proyecto importante
-la verdad fue un gran inicio pero creo solo me he quedado en el diseño y no en la funcionalidad que es lo mas importantes
+/* 
+pitacora dia 100 de este proyecto creo ya debiestar terminando
 */

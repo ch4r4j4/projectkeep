36a697bf918728d3c27837aa54213054dd66ee29
Paul Charaja Torres <68834265+ch4r4j4@users.noreply.github.com>
2024-04-02 23:31:34 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,4 +19,5 @@ export default App;
 /* 
 pitacora dia 100 de este proyecto creo ya debiestar terminando
 creo que ya casi lo tenemos listos segun vi el video
+tengo que ahora realizar bien la parte de nuestro login
 */

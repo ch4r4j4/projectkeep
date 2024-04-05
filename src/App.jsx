bfbdb877af744f034517d5b4fae49b585aa28272
Paul Charaja Torres <68834265+ch4r4j4@users.noreply.github.com>
2024-04-05 17:57:13 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,4 +22,5 @@ creo que ya casi lo tenemos listos segun vi el video
 tengo que ahora realizar bien la parte de nuestro login
 voy subeidno de una vez los cambios para que no aparezca vacio por hoy mi repositorio
 cada vez mas voy aprendiendo de como realiazr estos programas un poco mas de experiencia y ya casi estara listo
+ultimo cambio de la semana
 */

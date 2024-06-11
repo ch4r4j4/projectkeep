ceb50f2b01ba75c81627d8c8506ccc6ee8cef19f
Paul Charaja Torres <68834265+ch4r4j4@users.noreply.github.com>
2024-06-10 23:57:26 -0500

    cosas cambiadas

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import SignInSide from './components/SignInSide';
 import AddressForm from './components/AddressForm';
 import AppMenu from './components/AppMenu';
 import Checkout from './components/Checkout';
+import DeleteButton from './components/DeleteButton';
 
 const App=()=>{
     return (
-      <>
-      <AppMenu />
-
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <DeleteButton />
+        {/* <AppMenu />*/}
       {/*<AddressForm />*/}
       {/*<SignInSide />*/}
       
-      </>
+      </div>
+      
+  
     )
   }
 export default App;
